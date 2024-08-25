fe8a2ec85f59fef3087a9d8797f223fc3a3ca8aa
Ne184 <64256244+Ne184@users.noreply.github.com>
2024-08-25 09:05:31 +0200

    first final version of web application for ads budget calculations

---
+++ WebApplication1/Models/FileData.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class FileData
+    {
+        public string AdNumber { get; set; }
+        public double Budget { get; set; }
+
+        public FileData(string adNumber, int budget)
+        {
+            AdNumber = adNumber;
+            Budget = budget;
+        }
+        public FileData()
+        {
+        }
+    }
+}
